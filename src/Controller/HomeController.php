da6c807249c072d7b65110ecfe379b6378bcd022
dldvlpr <dormoy.dev@gmail.com>
2022-07-17 01:14:44 +0200

    git push

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\UserType;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -10,9 +14,22 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'home')]
-    public function index(): Response
+    public function index(Request $request, UserRepository $userRepository): Response
     {
-        return $this->render('home/index.html.twig', [
+        $user = new User();
+
+        $form = $this->createForm(UserType::class, $user);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted()) {
+            $userRepository->add($user, true);
+            return $this->redirectToRoute('app_home');
+        }
+
+        return $this->renderForm('home/index.html.twig', [
+            'form' => $form
         ]);
     }
+
 }
