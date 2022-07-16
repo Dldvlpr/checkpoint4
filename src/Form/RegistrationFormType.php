f771e873e11198eb4916b7b6430f936bec260fda
dldvlpr <dormoy.dev@gmail.com>
2022-07-17 01:55:48 +0200

    register login fixed

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -2,10 +2,13 @@
 
 namespace App\Form;
 
+use App\Entity\Game;
 use App\Entity\User;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
@@ -17,15 +20,14 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-            ->add('agreeTerms', CheckboxType::class, [
-                'mapped' => false,
-                'constraints' => [
-                    new IsTrue([
-                        'message' => 'You should agree to our terms.',
-                    ]),
-                ],
+            ->add('nickname', TextType::class)
+            ->add('games', EntityType::class, [
+                'class' => Game::class,
+                'choice_label' => 'name',
+                'multiple' => true,
+                'expanded' => true,
             ])
+            ->add('email')
             ->add('plainPassword', PasswordType::class, [
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
