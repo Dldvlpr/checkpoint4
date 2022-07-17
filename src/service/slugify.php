f5c6ee4f6edbf19b74b406113e92616315a8a9ed
dldvlpr <dormoy.dev@gmail.com>
2022-07-17 02:05:52 +0200

    slug service

---
+++ src/service/slugify.php
@@ -0,0 +1,16 @@
+<?php
+
+    namespace App\service;
+
+    class slugify
+    {
+        public function generate(string $input): string
+        {
+            $search = array('à', 'é', 'è', 'ç', '!', '-', ' ');
+            $replace = array('a', 'e', 'e', 'c','', '', '-');
+            $slug = preg_replace('/\s\s+/', '', $input);
+            $slug = strtolower(str_replace($search, $replace, $input));
+
+            return $slug;
+        }
+    }
