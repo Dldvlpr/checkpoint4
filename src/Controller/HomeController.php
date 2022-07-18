e91e1c8af6c8874155a6f37275ee8a79dc024991
dldvlpr <dormoy.dev@gmail.com>
2022-07-18 20:43:33 +0200

    i dont remember

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,6 +6,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 #[Route('/', name: 'app_')]
 class HomeController extends AbstractController
@@ -13,6 +14,7 @@ class HomeController extends AbstractController
     #[Route('/', name: 'home')]
     public function index(): Response
     {
+
         return $this->render('home/index.html.twig', [
         ]);
     }
