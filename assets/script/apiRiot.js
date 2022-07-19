4be1997fb9832c61e5d872a3315533c0f1d68caa
dldvlpr <dormoy.dev@gmail.com>
2022-07-19 21:56:38 +0200

    js vanilla call OK for dataID

--- assets/script/apiRiot.js
+++ assets/script/apiRiot.js
@@ -1,16 +1,29 @@
 const axios = require('axios');
 const urlApiLol = 'https://euw1.api.riotgames.com';
 axios.defaults.baseURL = urlApiLol;
-const apiRiotKey = 'RGAPI-7152edaf-2c82-4114-b480-d1c994c956db';
+const apiRiotKey = 'RGAPI-c6dc3ab4-e694-48cc-8d62-08557c8a48ce';
 
+// function getSummonerIdByPlayerName(playerName, apiRiotKey) {
+//
+//     axios.get('/lol/summoner/v4/summoners/by-name/' + playerName + '?api_key=' + apiRiotKey)
+//         .then(({data}) => {
+//             data.id
+//         }).catch(function (error) {
+//         return "Une erreur s'est produite merci de refaire votre recherche"
+//     })
+// }
+//
+// let Id = getSummonerIdByPlayerName('franckdes2b3', apiRiotKey)
+// console.log(Id)
 
 function getSummonerIdByPlayerName(playerName, apiRiotKey) {
-
-
-    const promise = axios.get('/lol/summoner/v4/summoners/by-name/' + playerName + '?api_key=' + apiRiotKey)
-    return promise.then((response) => response.data.id)
+    return fetch('https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/' + playerName + '?api_key=' + apiRiotKey)
+       .then(response => response.json())
+       .then(data => {
+           console.log(data)
+       })
+       .catch(() => alert('error'))
 }
 
-let toto = getSummonerIdByPlayerName('franckdes2b3', apiRiotKey).then(data=>{return data})
-
-console.log(toto)
+let Id = getSummonerIdByPlayerName('franckdes2b3', apiRiotKey)
+console.log(Id)
