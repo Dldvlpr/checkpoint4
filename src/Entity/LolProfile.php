a30ef8da920c1f59b4043479b7c987210a4d7d48
dldvlpr <dormoy.dev@gmail.com>
2022-07-20 09:26:05 +0200

    not null propr

--- src/Entity/LolProfile.php
+++ src/Entity/LolProfile.php
@@ -13,8 +13,53 @@ class LolProfile
     #[ORM\Column()]
     private ?int $id = null;
 
+    #[ORM\Column(length: 255)]
+    private ?string $leagueId = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $summonerId = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $summonerName = null;
+
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function getLeagueId(): ?string
+    {
+        return $this->leagueId;
+    }
+
+    public function setLeagueId(string $leagueId): self
+    {
+        $this->leagueId = $leagueId;
+
+        return $this;
+    }
+
+    public function getSummonerId(): ?string
+    {
+        return $this->summonerId;
+    }
+
+    public function setSummonerId(string $summonerId): self
+    {
+        $this->summonerId = $summonerId;
+
+        return $this;
+    }
+
+    public function getSummonerName(): ?string
+    {
+        return $this->summonerName;
+    }
+
+    public function setSummonerName(string $summonerName): self
+    {
+        $this->summonerName = $summonerName;
+
+        return $this;
     }
 }
