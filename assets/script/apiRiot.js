80fa87a53305487e67fded2decc0ae15d2a46d2c
dldvlpr <dormoy.dev@gmail.com>
2022-07-20 19:27:29 +0200

    api form

--- assets/script/apiRiot.js
+++ assets/script/apiRiot.js
@@ -1,14 +1,26 @@
 const urlApiLol = 'https://euw1.api.riotgames.com';
 const apiRiotKey = 'RGAPI-c6dc3ab4-e694-48cc-8d62-08557c8a48ce';
+let controller = new AbortController();
+setTimeout(() => controller.abort(), 1);
 
-function getSummonerIdByPlayerName(urlApiLol, playerName, apiRiotKey) {
-    return fetch(urlApiLol + '/lol/summoner/v4/summoners/by-name/' + playerName + '?api_key=' + apiRiotKey)
-        .then(response => response.json())
-        .then(data => {
-            console.log(data)
-        })
-        .catch(() => alert("Une erreur s'est produite merci de refaire votre recherche."))
-}
 
-let Id = getSummonerIdByPlayerName(urlApiLol, 'franckdes2b3', apiRiotKey)
-console.log(Id)
+document.querySelector("#playerName").addEventListener('input', function () {
+    for (let i = 3; i <= this.value.length; i++)
+        if (i < this.value.length) {
+            let url = urlApiLol + `/lol/summoner/v4/summoners/by-name/${this.value}?api_key=` + apiRiotKey;
+            fetch(url)
+                .then(response => response.json()
+                    .then(data => {
+                            let playerId = data.id;
+                            if (playerId != null) {
+                                let secondUrl = urlApiLol + `/lol/league/v4/entries/by-summoner/${playerId}?api_key=` + apiRiotKey;
+                                fetch(secondUrl).then((response) =>
+                                    response.json()
+                                        .then(playerData => console.log(playerData))
+                                )
+                            }
+                        }
+                    )
+                )
+        }
+})
