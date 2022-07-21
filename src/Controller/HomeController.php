4a583e9401a958796cb2167e03973a07003b6c70
dldvlpr <dormoy.dev@gmail.com>
2022-07-21 14:19:41 +0200

    api call sf

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 
+use App\Service\CallApiService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,10 +13,10 @@ use Symfony\Contracts\HttpClient\HttpClientInterface;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'home')]
-    public function index(): Response
+    public function index(CallApiService $callApiService): Response
     {
-
         return $this->render('home/index.html.twig', [
+
         ]);
     }
 
---
+++ src/Service/CallApiService.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Service;
+
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+
+
+class CallApiService
+{
+    private HttpClientInterface $client;
+
+    public function __construct(HttpClientInterface $client)
+    {
+        $this->client = $client;
+    }
+
+    public function getsummonerid(): array
+    {
+        $playerName = 'franckdes2b3';
+
+        $playerId = $this->client->request(
+            'GET',
+            "https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/$playerName?api_key=RGAPI-81da8750-b1f7-4283-b7e3-3a489e206628"
+        );
+
+        $ar = $playerId->toArray();
+        $id = strval($ar["id"]);
+
+
+        $playerStat = $this->client->request(
+            'GET',
+            "https://euw1.api.riotgames.com/lol/league/v4/entries/by-summoner/$id?api_key=RGAPI-81da8750-b1f7-4283-b7e3-3a489e206628"
+        );
+
+        return $playerStat = $playerStat->toArray();
+    }
+}
