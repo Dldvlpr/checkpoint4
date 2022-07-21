196395344d2b4ea9742873ca17d5dcdba6d48dea
dldvlpr <dormoy.dev@gmail.com>
2022-07-21 16:39:29 +0200

    api call sf

--- src/Controller/SearchController.php
+++ src/Controller/SearchController.php
@@ -4,29 +4,29 @@ namespace App\Controller;
 
 use App\Service\CallApiService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 #[Route('/search', name: 'search_')]
 class SearchController extends AbstractController
 {
-    #[Route('/', name: 'index')]
-    public function index(): Response
+    #[Route('/', name: 'index', methods: ['GET', 'POST'])]
+    public function index(Request $request): Response
     {
-
         return $this->render('search/index.html.twig', [
-
         ]);
     }
 
     #[Route('/search/{playerName}', name: 'player')]
     public function player(CallApiService $callApiService, string $playerName): Response
     {
+        $stat = $callApiService->getsummonerStat($playerName);
 
 
 
         return $this->render('search/searchBar.html.twig', [
-            'stat' => $callApiService->getsummonerStat($playerName),
+            'stat' => $stat
         ]);
     }
 }
